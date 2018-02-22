48d8e4810134c827466f48cdbb2d02c369067fac
NickMcG-PC <D11123772@mydit.ie>
2018-02-22 22:38:35 +0000

    lasers go everywhere

---
+++ Assets/Script/LaserButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserButton : MonoBehaviour
+{
+
+	public GameObject Laser;
+	private bool _coRouIsRunning = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Clicked()
+	{
+		if (!_coRouIsRunning)
+		StartCoroutine(LaserControl());
+	}
+
+	IEnumerator LaserControl()
+	{
+		_coRouIsRunning = true;
+		float t = 0;
+		Vector3 newScale = Laser.transform.localScale;
+		Quaternion myRotate = Quaternion.Euler(new Vector3(Random.Range(-45,45),Random.Range(-45,45),0));
+		Laser.transform.rotation = myRotate;
+		while (t < 2)
+		{
+			t += Time.deltaTime;
+			
+			// fire length
+			newScale.z += 5f * Time.deltaTime;
+			Laser.transform.localScale = newScale;
+			
+			//rotation
+			// need to rotate to the center of the screen
+			Laser.transform.rotation = Quaternion.RotateTowards(Laser.transform.rotation,Quaternion.identity,  20 * Time.deltaTime);
+			
+			yield return null;
+		}
+
+		newScale.z = 0;
+		Laser.transform.localScale = newScale;
+		_coRouIsRunning = false;
+	}
+}
---
+++ Assets/Script/SendClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+
+			if (Physics.Raycast(transform.position, transform.forward, out hit, 100000.0f))
+			{
+				if (hit.collider != null)
+				{
+					//print("looking at " + hit.collider);
+					hit.collider.gameObject.SendMessage("Clicked",SendMessageOptions.DontRequireReceiver);
+				}
+				
+			}
+		}
+	}
+}
