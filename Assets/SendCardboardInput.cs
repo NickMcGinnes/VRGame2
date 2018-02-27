1e0cbc95378989d0b12861fe2bfbaaf5aeef0d3c
NickMcG-PC <D11123772@mydit.ie>
2018-02-27 16:30:12 +0000

    stuff

---
+++ Assets/SendCardboardInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendCardboardInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			RaycastHit hit;
+
+			if (Physics.Raycast(transform.position, transform.forward, out hit, 100000.0f))
+			{
+				if (hit.collider != null)
+				{
+					//print("looking at " + hit.collider);
+					hit.collider.gameObject.SendMessage("Clicked",SendMessageOptions.DontRequireReceiver);
+				}
+				
+			}
+		}
+	}
+}
