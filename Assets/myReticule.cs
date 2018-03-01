65e3ad974c9867de6d17219aa4b9a380822c8e3c
NickMcG-PC <D11123772@mydit.ie>
2018-03-01 19:58:36 +0000

    cube where i look

---
+++ Assets/myReticule.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myReticule : MonoBehaviour
+{
+	public GameObject Cube;
+
+	private Vector3 origin;
+	// Use this for initialization
+	void Start ()
+	{
+		origin = Cube.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit, 20.0f))
+		{
+			if (hit.collider != null)
+			{
+				Cube.transform.position = hit.point;
+			}
+		}
+		else
+		{
+			Cube.transform.localPosition = origin;
+		}
+	}
+}
