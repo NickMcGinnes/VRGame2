497d47482e80bb226c47ac7eb3d66f35ef3f5db5
Jack <c15729315@mydit.ie>
2018-03-02 19:52:45 +0000

    Alarm and Confetti buttons

---
+++ Assets/Scripts/AlarmButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmButton : MonoBehaviour 
+{
+    public AudioClip[] buttonSounds;
+    public AudioSource buttonAudioSource;
+    bool isRunning = false;
+    public GameObject alarm;
+
+    // Use this for initialization
+    void Start () 
+	{
+        buttonAudioSource = gameObject.GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    void Clicked()
+    {
+        if (!isRunning)
+        {
+            alarm.SetActive(true);
+        }
+
+        buttonAudioSource.clip = buttonSounds[Random.Range(0, buttonSounds.Length)];
+        buttonAudioSource.Play();
+    }
+}
---
+++ Assets/Scripts/RandomButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomButton : MonoBehaviour 
+{
+    public AudioClip[] buttonSounds;
+    public AudioSource buttonAudioSource;
+    bool isRunning = false;
+
+    // Use this for initialization
+    void Start () 
+	{
+        buttonAudioSource = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    void Clicked()
+    {
+        if (!isRunning)
+        {
+            
+        }
+
+        buttonAudioSource.clip = buttonSounds[Random.Range(0, buttonSounds.Length)];
+        buttonAudioSource.Play();
+    }
+}
